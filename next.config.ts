c5554ad0f230912e464b38da8c127626ca8c4aba
kelompok 1 <fyznugraha@gmail.com>
2026-07-15 22:52:21 +0700

    Config: Remove /scan rewrite, using subdomain scan.byfayiz.web.id instead

--- next.config.ts
+++ next.config.ts
@@ -3,24 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/portofolio',
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/scan',
-          destination: 'https://scanner-eta-two.vercel.app/',
-          basePath: false,
-        },
-        {
-          source: '/scan/:path*',
-          destination: 'https://scanner-eta-two.vercel.app/:path*',
-          basePath: false,
-        },
-      ],
-      afterFiles: [],
-      fallback: [],
-    };
-  },
 };
 
 export default nextConfig;
